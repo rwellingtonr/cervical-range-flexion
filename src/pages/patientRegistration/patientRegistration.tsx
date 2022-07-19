fd8b9c449644a1904156af6df493918c412c39c7
Wellington Leardini <wellington-158@hotmail.com>
2022-07-18 22:16:23 -0300

    histórico de paciente

--- src/pages/patientRegistration/patientRegistration.tsx
+++ src/pages/patientRegistration/patientRegistration.tsx
@@ -4,6 +4,9 @@ import style from "./patientRegistration.module.scss"
 import DefaultButton from "../../components/defaultButton.ts/defaultButton"
 import { FormControl, InputLabel, MenuItem, Select, TextField } from "@mui/material"
 import { api } from "../../service/api"
+import CustomizedSnackbars from "../../components/alert"
+import InputDate from "../../components/inputDate/inputDate"
+import { Person } from "@mui/icons-material"
 
 type Patient = {
     cpf: string
@@ -12,26 +15,31 @@ type Patient = {
     surgery_date: string
     birthday: string
 }
+type FeedBack = {
+    severity: "error" | "info" | "success" | "warning"
+    message: string
+    open: boolean
+}
 
 const inputStyle = {
     m: 1,
     borderRadius: "20px",
     background: "rgba(143, 218, 200, 0.2)",
-    "& .MuiOutlinedInput-root": { borderRadius: "20px" },
 }
 
-const dateStyles = { ...inputStyle, width: 250 }
-
 const genders = ["masculino", "feminino", "outro"]
-
-const defaultDateValue = "1990-01-01"
 
 function PatientRegistration() {
     const [name, setName] = useState("")
     const [cpf, setCpf] = useState("")
     const [gender, setGender] = useState("")
-    const [surgeryDate, setSurgeryDate] = useState(defaultDateValue)
-    const [birthday, setBirthday] = useState(defaultDateValue)
+    const [surgeryDate, setSurgeryDate] = useState("")
+    const [birthday, setBirthday] = useState("")
+    const [feedback, setFeedBack] = useState<FeedBack>({
+        open: false,
+        message: "",
+        severity: "success",
+    })
 
     const handleSubmit = async (e: FormEvent) => {
         e.preventDefault()
@@ -48,18 +56,30 @@ function PatientRegistration() {
 
             const res = await api.post("/patient", patient)
             cleanUp()
-            alert(res.data)
+            setFeedBack({
+                open: true,
+                message: `paciente criado ${res.data.name} com sucesso`,
+                severity: "success",
+            })
         } catch (err) {
             console.error(err)
+            setFeedBack({ open: true, message: "Erro ao criar o paciente", severity: "error" })
         }
+    }
+    const handleClose = (event?: React.SyntheticEvent | Event, reason?: string) => {
+        if (reason === "clickaway") {
+            return
+        }
+
+        setFeedBack({ ...feedback, open: false })
     }
 
     const cleanUp = () => {
         setName("")
         setCpf("")
         setGender("")
-        setSurgeryDate(defaultDateValue)
-        setBirthday(defaultDateValue)
+        setSurgeryDate("")
+        setBirthday("")
     }
 
     const handleSetDate = (type: string, date: string) => {
@@ -73,6 +93,12 @@ function PatientRegistration() {
 
     return (
         <main className={style.divWrapper}>
+            <CustomizedSnackbars
+                message={feedback.message}
+                severity={feedback.severity}
+                handleClose={handleClose}
+                open={feedback.open}
+            />
             <Box
                 component="form"
                 className={style.formWrapper}
@@ -92,7 +118,7 @@ function PatientRegistration() {
                 <TextField
                     required
                     fullWidth
-                    sx={inputStyle}
+                    sx={{ ...inputStyle, "& .MuiOutlinedInput-root": { borderRadius: "20px" } }}
                     id="outlined-required"
                     value={name}
                     label="Nome"
@@ -102,33 +128,25 @@ function PatientRegistration() {
                 />
                 <div className={style.lastRow}>
                     {" "}
-                    <TextField
-                        id="date"
-                        label="Data de nascimento"
-                        type="date"
-                        value={birthday}
-                        sx={dateStyles}
-                        onChange={e => handleSetDate("birthday", e.target.value)}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
+                    <InputDate
+                        handleAction={handleSetDate}
+                        name={"birthday"}
+                        label={"Data de nascimento"}
                     />
-                    <TextField
-                        id="date"
-                        label="Data da cirurgia"
-                        type="date"
-                        value={surgeryDate}
-                        onChange={e => handleSetDate("surgery", e.target.value)}
-                        sx={dateStyles}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
+                    <InputDate
+                        handleAction={handleSetDate}
+                        name={"surgery"}
+                        label={"Data da cirurgia"}
                     />
                 </div>
                 <div className={style.lastRow}>
                     <TextField
                         required
-                        sx={{ ...inputStyle, width: 280 }}
+                        sx={{
+                            ...inputStyle,
+                            "& .MuiOutlinedInput-root": { borderRadius: "20px" },
+                            width: 280,
+                        }}
                         id="outlined-required"
                         value={cpf}
                         label="CPF"
@@ -138,7 +156,13 @@ function PatientRegistration() {
                         onChange={e => setCpf(e.target.value)}
                     />
                     <>
-                        <FormControl sx={{ width: 170, ...inputStyle }}>
+                        <FormControl
+                            sx={{
+                                width: 170,
+                                ...inputStyle,
+                                "& .MuiOutlinedInput-root": { borderRadius: "20px" },
+                            }}
+                        >
                             <InputLabel id="demo-simple-select-label">Gênero</InputLabel>
                             <Select
                                 labelId="demo-simple-select-label"
@@ -157,7 +181,13 @@ function PatientRegistration() {
                         </FormControl>
                     </>
                 </div>
-                <DefaultButton handleClick={console.log}>Cadastrar</DefaultButton>
+                <div className={style.submit}>
+                    <DefaultButton handleClick={console.log}>
+                        {" "}
+                        <Person style={{ paddingRight: "10px" }} />
+                        Cadastrar
+                    </DefaultButton>
+                </div>
             </Box>
         </main>
     )
