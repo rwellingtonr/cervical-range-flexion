d609a897ef7ef40c173e976b111fc9eb56593499
Wellington Leardini <wellington-158@hotmail.com>
2022-07-18 22:15:57 -0300

    adicionado feedback

--- src/components/alert.tsx
+++ src/components/alert.tsx
@@ -1,57 +1,37 @@
 import * as React from "react"
-import Alert from "@mui/material/Alert"
-import AlertTitle from "@mui/material/AlertTitle"
 import Stack from "@mui/material/Stack"
+import Button from "@mui/material/Button"
+import Snackbar from "@mui/material/Snackbar"
+import MuiAlert, { AlertProps } from "@mui/material/Alert"
 
-interface IAlert {
-    type: Alerts
+type Props = {
+    severity: "error" | "info" | "success" | "warning"
     message: string
+    handleClose: (event?: React.SyntheticEvent | Event, reason?: string) => void
+    open: boolean
 }
 
-type Alerts = "error" | "warning" | "success" | "info"
+const Alert = React.forwardRef<HTMLDivElement, AlertProps>(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="standard" {...props} />
+})
 
-export default function DescriptionAlerts({ type, message }: IAlert) {
-    const selectAlert = (type: Alerts) => {
-        switch (type) {
-            case "error":
-                return (
-                    <Alert severity="error">
-                        <AlertTitle>Erro</AlertTitle>
-                        {message} — <strong>Revise</strong>
-                    </Alert>
-                )
+const vertical = "bottom"
+const horizontal = "right"
 
-            case "info":
-                return (
-                    <Alert severity="info">
-                        <AlertTitle>Informação</AlertTitle>
-                        {message} — <strong>Informativo!</strong>
-                    </Alert>
-                )
-
-            case "warning":
-                return (
-                    <Alert severity="warning">
-                        <AlertTitle>Alerta</AlertTitle>
-                        {message} — <strong>Atenção!</strong>
-                    </Alert>
-                )
-            case "success":
-                return (
-                    <Alert severity="success">
-                        <AlertTitle>Sucesso</AlertTitle>
-                        {message} — <strong>OK</strong>
-                    </Alert>
-                )
-
-            default:
-                return <></>
-        }
-    }
-
+export default function CustomizedSnackbars({ severity, message, open, handleClose }: Props) {
     return (
-        <Stack sx={{ width: "100%" }} spacing={2}>
-            {selectAlert(type)}
-        </Stack>
+        <>
+            <Snackbar
+                open={open}
+                autoHideDuration={3000}
+                onClose={handleClose}
+                key={vertical + horizontal}
+                anchorOrigin={{ vertical, horizontal }}
+            >
+                <Alert onClose={handleClose} severity={severity} sx={{ width: "100%" }}>
+                    {message}
+                </Alert>
+            </Snackbar>
+        </>
     )
 }
