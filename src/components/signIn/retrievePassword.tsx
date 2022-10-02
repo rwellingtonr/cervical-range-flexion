0f354edae94f409a38ef5ff4b3922dfe5f4cd78a
Wellington Leardini <wellington-158@hotmail.com>
2022-10-02 18:53:47 -0300

    feat: redefinição de senha

--- src/components/signIn/retrievePassword.tsx
+++ src/components/signIn/retrievePassword.tsx
@@ -5,26 +5,54 @@ import InputTxt from "./inputTxt"
 import style from "./sign.module.scss"
 import InputPassword from "./inputPassword"
 import SignInButton from "./buttonModal/signInButton"
+import { api } from "../../service/api"
+import { useAlert } from "../../context/alert"
+import CustomizedSnackbars from "../alert/alert"
+import { AxiosError } from "axios"
 
-export default function RetrievePassword() {
+type RetrievePasswordProps = {
+	validateCrefito: (crefito: string) => boolean
+}
+
+export default function RetrievePassword({ validateCrefito }: RetrievePasswordProps) {
 	const [crefito, setCrefito] = useState<string>("")
 	const [password, setPassword] = useState<string>("")
 	const navigate = useNavigate()
+	const { handleAlert } = useAlert()
 
 	const handleNewPassword = async () => {
 		try {
-			navigate("/measurement", { replace: true })
-		} catch (error) {
-			console.error(error)
+			const isValidCrefito = validateCrefito(crefito)
+			if (!isValidCrefito) return handleAlert("Crefito inválido", "warning")
+			if (!password.trim()) return handleAlert("Senha inválida", "warning")
+			await api.patch(`/physiotherapist/${crefito}`, { password })
+			navigate("/sign/login", { replace: true })
+		} catch (err) {
+			console.error(err)
+			if (err instanceof AxiosError) return handleAxiosError(err.response?.status)
+
+			handleAlert("Verifique os dados de entrada")
 		}
+	}
+	function handleAxiosError(status?: number) {
+		if (status === 404) return handleAlert("Usuário não encontrado!")
+		handleAlert("Senha deve ser diferente da anterior!")
 	}
 
 	const handleKeyPress = async (event: React.KeyboardEvent<HTMLFormElement>) => {
-		if (event.key === "Enter") return await handleNewPassword()
+		if (event.key === "Enter") {
+			event.preventDefault()
+			return await handleNewPassword()
+		}
+	}
+	const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+		event.preventDefault()
+		await handleNewPassword()
 	}
 
 	return (
 		<div className={style.formWrapper}>
+			<CustomizedSnackbars />
 			<Box
 				component="form"
 				className={style.form}
@@ -37,13 +65,19 @@ export default function RetrievePassword() {
 				}}
 				noValidate
 				autoComplete="off"
+				onSubmit={handleSubmit}
 				onKeyPress={handleKeyPress}
 			>
 				<div>
 					<h1>Nova Senha</h1>
 				</div>
 
-				<InputTxt label="Crefito" placeHolder="Informe seu Crefito" fillIn={setCrefito} />
+				<InputTxt
+					label="Crefito"
+					value={crefito}
+					placeHolder="Informe seu Crefito, exemplo: 8123456TO"
+					fillIn={setCrefito}
+				/>
 
 				<InputPassword setPassword={setPassword} password={password} />
 
