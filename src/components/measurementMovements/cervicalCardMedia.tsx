2b52bbe6c486be606f6e5d97dbeb208d45610984
Wellington Leardini <wellington-158@hotmail.com>
2022-11-19 11:47:06 -0300

    feat: troca de imagens

--- src/components/measurementMovements/cervicalCardMedia.tsx
+++ src/components/measurementMovements/cervicalCardMedia.tsx
@@ -2,27 +2,11 @@ import React from "react"
 import Card from "@mui/material/Card"
 import CardMedia from "@mui/material/CardMedia"
 import CardContent from "@mui/material/CardContent"
-
-// images
-import lateralLeft from "../../assets/lateral esquerda.png"
-import lateralRight from "../../assets/lateral direita.png"
-import flexion from "../../assets/flexão.png"
-import { Movement } from "../../interface/movement"
+import { cervicalMovements } from "./cervicalMovements"
+import type { Movement } from "../../interface/movement"
 
 type CervicalCardMediaProps = {
 	movement: Movement
-}
-
-const cervicalMovements = {
-	"lateral-right": {
-		src: lateralRight,
-		text: "O paciente deve realizado o movimento de flexão à Rotação lateral direita",
-	},
-	"lateral-left": {
-		src: lateralLeft,
-		text: "O paciente deve realizado o movimento de flexão à Rotação lateral esquerda",
-	},
-	flexion: { src: flexion, text: "O paciente deve realizado o movimento de Flexão frontal" },
 }
 
 export default function CervicalCardMedia({ movement }: CervicalCardMediaProps) {
---
+++ src/components/measurementMovements/cervicalMovements.ts
@@ -0,0 +1,16 @@
+// images
+import lateralLeft from "../../assets/Rotacao esquerda.png"
+import lateralRight from "../../assets/Rotacao direita.png"
+import flexion from "../../assets/flexão.png"
+
+export const cervicalMovements = {
+	"lateral-right": {
+		src: lateralRight,
+		text: "O paciente deve realizado o movimento de rotação à lateral direita",
+	},
+	"lateral-left": {
+		src: lateralLeft,
+		text: "O paciente deve realizado o movimento de rotação à lateral esquerda",
+	},
+	flexion: { src: flexion, text: "O paciente deve realizado o movimento de Flexão frontal" },
+}
